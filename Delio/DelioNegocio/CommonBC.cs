cec9396a8b2268936829ea3c35e138d4d8779522
AL_ON_PC <alon.berrios@gmail.com>
2017-06-14 14:01:37 -0400

    Project start

---
+++ Delio/DelioNegocio/CommonBC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelioDALC;
+using System.Data.Entity;
+
+namespace DelioConexion
+{
+    public class CommonBC
+    {
+        private static Delio_INSEntities modeloDelio;
+
+        public static Delio_INSEntities ModeloDelio
+        {
+            get
+            {
+                if (modeloDelio == null)
+                {
+                    modeloDelio = new Delio_INSEntities();
+                }
+                return modeloDelio;
+            }
+        }
+
+        public CommonBC()
+        {
+
+        }
+    }
+}
---
+++ Delio/DelioNegocio/CuentaUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelioNegocio
+{
+    public class CuentaUsuario
+    {
+        public string IdCuentaUsuario { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public char Sexo { get; set; }
+        public int Telefono { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public string Email { get; set; }
+        public int IdTipoCuenta { get; set; }
+    }
+}
